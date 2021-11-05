714fe0d10adaf3d98c9a0cfd6cd67d18885ed51a
starkyb <yb010626@naver.com>
2021-11-05 15:57:30 +0900

    [김유빈] - 파일 배포 코드 수정

--- src/main/java/com/sportstelling/config/WebMvcConfig.java
+++ src/main/java/com/sportstelling/config/WebMvcConfig.java
@@ -17,7 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**")
-		.addResourceLocations("file:/" + FileManagerService.FILE_UPLOAD_PATH);
+		.addResourceLocations("file:" + FileManagerService.FILE_UPLOAD_PATH);
 	}
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
